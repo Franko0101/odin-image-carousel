775d63f04917e31baf085e2a73ef4235bacde75e
Franko0101 <163135894+Franko0101@users.noreply.github.com>
2025-08-12 19:01:04 +0200

    Project completed.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@ import { greeting } from "./template.js";
 //import image from "./image.png";
 
 console.log(greeting);
+
+let imageTimer = window.setInterval(nextImage, 5000);
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const carouselSlide = document.querySelector(".carousel-slide");
+let left = 0;
+
+prevBtn.addEventListener("click", () => {
+    left = left != 0 ? left+1536 : -6144;
+    carouselSlide.style.left = `${left}px`;
+    window.clearInterval(imageTimer);
+    imageTimer = window.setInterval(nextImage, 5000);
+});
+
+function nextImage() {
+    left = left != -6144 ? left-1536 : 0;
+    carouselSlide.style.left = `${left}px`;
+    window.clearInterval(imageTimer);
+    imageTimer = window.setInterval(nextImage, 5000);
+}
+
+nextBtn.addEventListener("click", nextImage);
